b9f81ee2e42dc03462847f19634d2b983493c3db
GitHub Action <michael+cicd@offisito.com>
2024-05-15 19:36:08 +0300

    finalizations

--- src/api/routes/users/index.ts
+++ src/api/routes/users/index.ts
@@ -1,13 +1,71 @@
-import {Router} from 'express';
-import {getAllUsers, getUserByEmail, getUserByName, updateStatuses} from "../../../controllers/users";
-import {removeUserFromGroup} from "../../../controllers/groups";
+import {Router} from "express";
+import {
+    getAllUsers,
+    getUsersByEmail,
+    getUsersByName,
+    removeUserFromGroup,
+    updateStatuses
+} from "../../../controllers/users";
 
 const router = Router();
 
-router.get("/all/:limit/:offset", getAllUsers);
-router.get("/byName/:name", getUserByName);
-router.get("/byEmailAddress/:email", getUserByEmail);
-router.patch("/statuses", updateStatuses);
-router.delete("/userFromGroup/:userId", removeUserFromGroup);
+router.get("/all", async (req, res, next) => {
+    try {
+        const limit = parseInt(req.query.limit as string) || 10;
+        const offset = parseInt(req.query.offset as string) || 0;
+        const users = await getAllUsers(limit, offset);
+        return res.status(200).json(users);
+    } catch (e) {
+        next(e);
+    }
+});
+
+router.get("/byName/:name", async (req, res, next) => {
+    try {
+        const name = req.params.name;
+        if (!name) return res.status(400).send("Name is required");
+        const users = await getUsersByName(name);
+        return res.status(200).json(users);
+    } catch (e) {
+        next(e);
+    }
+});
+
+router.get("/byEmailAddress/:email", async (req, res, next) => {
+    try {
+        const email = req.params.email;
+        if (!email) return res.status(400).send("Email Address is required");
+        const users = await getUsersByEmail(email);
+        return res.status(200).json(users);
+    } catch (e) {
+        next(e);
+    }
+});
+
+router.patch("/statuses", async (req, res, next) => {
+    const updates = req.body.updates;
+    if (!updates || updates.length === 0) {
+        return res.status(400).send("Updates are required");
+    }
+    //I would add zod to further validate the input, or utilize typescript even more if its inside a monorepo with the frontend
+    try {
+        await updateStatuses(updates);
+        res.status(201).send("Updates successful");
+    } catch (e) {
+        next(e);
+    }
+});
+
+router.delete("/userFromGroup/:userId", async (req, res, next) => {
+    try {
+        const userId = req.params.userId;
+        await removeUserFromGroup(userId);
+        return res.status(201).send("Success");
+    } catch (e) {
+        const non500Error = JSON.parse(e as string)
+        if (non500Error) return res.status(non500Error.code).send(non500Error.message)
+        else next(e);
+    }
+});
 
 export default router;
--- src/controllers/users.ts
+++ src/controllers/users.ts
@@ -1,75 +1,82 @@
-import {Request, Response, NextFunction} from 'express';
 import {UserStatus} from "../services/mongo/schemas/users";
-import {getUserModel} from "../services/mongo/models";
+import {getGroupModel, getUserModel} from "../services/mongo/models";
 import {getConnection} from "../services/mongo";
+import {isValidObjectId} from "mongoose";
+import {updateGroupStatus} from "./groups";
 
 interface StatusUpdate {
     userId: string;
     newStatus: UserStatus;
 }
 
-export const getAllUsers = async (req: Request, res: Response, next: NextFunction) => {
-    try {
-        const limit = parseInt(req.params.limit) || 10;
-        const skip = parseInt(req.params.offset) || 0;
-        const Users = getUserModel();
-        const allUsers = await Users.find().limit(limit).skip(skip).lean();
-        res.status(200).json(allUsers);
-    } catch (e) {
-        next(e);
-    }
-}
+export const getAllUsers = (limit: number, offset: number) => {
+    const Users = getUserModel();
+    return Users.find().limit(limit).skip(offset).lean();
+};
 
-export const getUserByName = async (req: Request, res: Response, next: NextFunction) => {
-    try {
-        const name = req.params.name;
-        if (!name) return res.status(400).send("Name is required");
-        const Users = getUserModel();
-        const usersResult = await Users.find({name}).lean();
-        res.status(200).json(usersResult);
-    } catch (e) {
-        next(e);
-    }
-}
+export const getUsersByName = (name: string) => {
+    const Users = getUserModel();
+    return Users.find({name}).lean();
+};
 
-export const getUserByEmail = async (req: Request, res: Response, next: NextFunction) => {
-    try {
-        const email = req.params.email;
-        if (!email) return res.status(400).send("Email Address is required");
-        const Users = getUserModel();
-        const usersResult = await Users.find({email}).lean();
-        res.status(200).json(usersResult);
-    } catch (e) {
-        next(e);
-    }
-}
+export const getUsersByEmail = (email: string) => {
+    const Users = getUserModel();
+    return Users.find({email}).lean();
+};
 
-export const updateStatuses = async (req: Request, res: Response, next: NextFunction) => {
+export const updateStatuses = async (updates: StatusUpdate[]) => {
+    const Users = getUserModel();
+    const session = await getConnection().startSession();
     try {
-        const updates: StatusUpdate[] = req.body.updates;
-        if (!updates || updates.length === 0) {
-            return res.status(400).send("Updates are required");
-        }
-        const Users = getUserModel();
-        const session = await getConnection().startSession(); // no need to check if db is non-null because the model exists and the whole express is set up after db connection
         session.startTransaction();
-        try {
-            const bulkOps = updates.map(({userId, newStatus}) => ({
-                updateOne: {
-                    filter: {_id: userId},
-                    update: {$set: {status: newStatus}}
-                }
-            }));
-            await Users.bulkWrite(bulkOps);
-            await session.commitTransaction();
-        } catch (error) {
-            await session.abortTransaction();
-            res.status(500).send(`Update failed: ${error}`);
-        } finally {
-            await session.endSession();
-        }
-        res.status(201).send("Updates successful");
+        const bulkOps = updates.map(({userId, newStatus}) => ({
+            updateOne: {
+                filter: {_id: userId},
+                update: {$set: {status: newStatus}},
+            },
+        }));
+        await Users.bulkWrite(bulkOps, {session, ordered: false});
+        await session.commitTransaction();
+        return {success: true};
     } catch (error) {
-        next()
+        await session.abortTransaction();
+        throw error;
+    } finally {
+        await session.endSession();
     }
-}
+};
+
+export const removeUserFromGroup = async (userId: string) => {
+    if (!isValidObjectId(userId)) throw new Error(JSON.stringify({
+        code: 400,
+        message: "A valid user ID is required"
+    }));
+    
+    const Users = getUserModel();
+    const Groups = getGroupModel();
+    const user = await Users.findById(userId);
+
+    if (!user) throw new Error(JSON.stringify({code: 404, message: "No user found for given user ID"}));
+    if (!user.associatedGroup) throw new Error(JSON.stringify({
+        code: 400,
+        message: "This user is already not associated with any group"
+    }));
+    const group = await Groups.findById(user.associatedGroup);
+
+    const session = await getConnection().startSession();
+    session.startTransaction();
+    try {
+        user.associatedGroup = undefined;
+        await user.save();
+        group && await updateGroupStatus(group)
+
+        await session.commitTransaction();
+        return {success: true};
+    } catch (error) {
+        await session.abortTransaction();
+        throw error;
+    } finally {
+        await session.endSession();
+    }
+};
+
